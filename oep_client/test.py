0bc7b41305f807968546e6383438915a579fb1a6
Christian Winger <c@wingechr.de>
2020-04-03 09:57:24 +0200

    test script main

--- oep_client/test.py
+++ oep_client/test.py
@@ -1,7 +1,11 @@
 # coding: utf-8
 import datetime
 from .client import OepClient
+from .script import setup_logging
 import pandas as pd
+import random
+from argparse import ArgumentParser
+
 
 example_metadata = {
     "id": "test",
@@ -28,7 +32,7 @@ example_data = pd.DataFrame([
 
 def test(token):
     timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    tablename = 'test_' + timestamp
+    tablename = 'test_%s_%d'  % (timestamp, random.randint(100000, 999999)) 
     cl = OepClient(token=token, tablename=tablename)
     cl.create(metadata=example_metadata)
     try:
@@ -40,3 +44,17 @@ def test(token):
         print(metadata)
     finally:
         cl.delete()
+
+if __name__ == "__main__":
+    ap = ArgumentParser()
+    ap.add_argument("token", help="user token")
+    ap.add_argument(
+        "--loglevel",
+        "-l",
+        default="INFO",
+        type=str,
+        help="ERROR, WARNING, INFO, or DEBUG",
+    )
+    kwargs = vars(ap.parse_args())
+    setup_logging(kwargs.pop("loglevel"))
+    test(**kwargs)
