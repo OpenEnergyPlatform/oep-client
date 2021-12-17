b922d2888b52cb52d8c4d4362864eb927606d1e2
c.winger <c.winger@oeko.de>
2021-12-17 14:11:38 +0100

    fix exception

--- oep_client/exceptions.py
+++ oep_client/exceptions.py
@@ -15,11 +15,11 @@ class OepAuthenticationException(OepClientSideException):
 
 
 class OepTableNotFoundException(OepClientSideException):
-    def __init__(self, _msg):
+    def __init__(self, _msg=None):
         # the API falsely returns message: {'detail': 'You do not have permission to perform this action}
         # but this is only because table  does not exist
         # TODO: create better error message on server side!
-        super().__init__("Table does not exist OR you don't have permission")
+        super().__init__(_msg or "Table does not exist OR you don't have permission")
 
 
 class OepTableAlreadyExistsException(OepClientSideException):
