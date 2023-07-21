e7d5ea7b89dfbd29440298e6307a176e1a97a4ce
c.winger <c.winger@oeko.de>
2023-07-21 15:28:59 +0200

    insert dataframe

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="oep-client",
-    version="0.15.0",
+    version="0.16.0",
     description="client side tool for openenergy platform",
     long_description_content_type="text/markdown",
     long_description=long_description,
